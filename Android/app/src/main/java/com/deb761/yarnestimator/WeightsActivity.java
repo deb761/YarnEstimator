c6c9b18d6e041ef7631f137bb3f5b454fe0633ec
Deborah Engelmeyer <dengelmeyer@icloud.com>
2016-04-29 04:01:49 -0700

    Modified app bar to match design
    Changed font color and size.  Alignment not changing.  Also changed
    text on Weights page and removed second bar.

--- Android/app/src/main/java/com/deb761/yarnestimator/WeightsActivity.java
+++ Android/app/src/main/java/com/deb761/yarnestimator/WeightsActivity.java
@@ -3,6 +3,7 @@ package com.deb761.yarnestimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -20,6 +21,7 @@ public class WeightsActivity extends AppCompatActivity {
         ListView listView = (ListView)findViewById(R.id.weightsListView);
         listView.setAdapter(adapter);
 
+        getSupportActionBar().setTitle("Standard Yarn Weights");
         initHomeButton();
         initInfoButton();
     }
